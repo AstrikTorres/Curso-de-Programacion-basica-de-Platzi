8ed9d6c047a5db4dfc3d25e28f88be18dcbfa8db
Astrik Torres <astriktorres@gmail.com>
2021-06-04 08:01:23 -0500

    Agregue la funcion "esDivisible"

--- fizzbuzz/fizz.js
+++ fizzbuzz/fizz.js
@@ -1,20 +1,32 @@
 var numeros = 100;
 for(var i = 1; i <= 100; i++)
 {
-    if(i % 3 == 0)
+    if(esDivisible(i,3))
     {
         document.write('fizz');
     }
     
-    if(i % 5 == 0)
+    if(esDivisible(i,5))
     {
         document.write('buzz');
     }
 
-    if(i % 3 != 0 && i % 5 != 0)
+    if(!esDivisible(i,3) && !esDivisible(i,5))
     {
         document.write(i);
     }
 
     document.write('<br/>')
+}
+
+function esDivisible(num, divisor)
+{
+    if(num % divisor == 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
