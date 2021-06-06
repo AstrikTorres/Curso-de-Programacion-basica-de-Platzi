c4733461506080bd3db95b88796123deefc0698c
Astrik Torres <astriktorres@gmail.com>
2021-06-06 17:30:15 -0500

    Funcion mostrar añadida en clase Pakiman

--- pakiman/pakiman.js
+++ pakiman/pakiman.js
@@ -7,17 +7,30 @@ class Pakiman
 {
     constructor(n, v, a)
     {
+        this.imagen = new Image
         this.nombre = n;
         this.vida = v;
         this.ataque = a;
+
+        this.imagen.src = imagenes[this.nombre]
     }
     hablar()
     {
         alert(this.nombre);
+    }
+    mostrar()
+    {
+        document.write('<p>');
+        document.body.appendChild(this.imagen);
+        document.write('<strong>' + this.nombre +'</strong><br />');
+        document.write('Vida: ' + this.vida +'<br />');
+        document.write('Ataque: ' + this.ataque);
+        document.write('</p>');
     }
 }
 
 var cauchin = new Pakiman('Cauchin', 100, 30);
 var pokacho = new Pakiman('Pokacho', 80, 50);
 var tocinauro = new Pakiman('Tocinauro', 120, 40);
-console.log(cauchin, pokacho, tocinauro);
+
+pokacho.mostrar();
