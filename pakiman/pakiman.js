d7fd4b65af9cf132176d2ad336a99f888ca8eaed
Astrik Torres <astriktorres@gmail.com>
2021-06-06 17:09:09 -0500

    Se añadio class Pakiman

---
+++ pakiman/pakiman.js
@@ -0,0 +1,18 @@
+class Pakiman
+{
+    constructor(n, v, a)
+    {
+        this.nombre = n;
+        this.vida = v;
+        this.ataque = a;
+    }
+    hablar()
+    {
+        alert(this.nombre);
+    }
+}
+
+var cauchin = new Pakiman('Cauchin', 100, 30);
+var pokacho = new Pakiman('Pokacho', 80, 50);
+var tocinauro = new Pakiman('Tocinauro', 120, 40);
+console.log(cauchin, pokacho, tocinauro);
