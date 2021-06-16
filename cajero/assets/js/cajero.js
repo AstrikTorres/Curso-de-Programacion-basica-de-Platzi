3d45223282da3b979ff8281b6d0fe91c736d3867
Astrik Torres <astriktorres@gmail.com>
2021-06-16 15:08:52 -0500

    Ahora muestra los billetes

--- cajero/assets/js/cajero.js
+++ cajero/assets/js/cajero.js
@@ -4,29 +4,38 @@ class Billete
     {
         this.valor = v;
         this.cantidad = c;
+        this.imagen = new Image();
+        this.imagen.src = imagenes[this.valor];
     }
 }
+
+var imagenes = [];
+imagenes["500"] = "assets/images/500pesos.jpg";
+imagenes["200"] = "assets/images/200pesos.jpg";
+imagenes["100"] = "assets/images/100pesos.jpg";
+imagenes["50"] = "assets/images/50pesos.jpg";
+imagenes["20"] = "assets/images/20pesos.jpg";
 
 function entregarDinero()
 {
     var t = document.getElementById('dinero');
     dinero = parseInt(t.value);
-    for(var bi of caja)
+    for(var b of caja)
     {
         if(dinero > 0)
         {
-            div = Math.floor(dinero / bi.valor);
-            if (div > bi.cantidad)
+            div = Math.floor(dinero / b.valor);
+            if (div > b.cantidad)
             {
-                papeles = bi.cantidad
+                papeles = b.cantidad
             }
             else
             {
                 papeles = div;
             }
             
-            entregado.push (new Billete(bi.valor, papeles) );
-            dinero = dinero - (bi.valor * papeles);
+            entregado.push (new Billete(b.valor, papeles) );
+            dinero = dinero - (b.valor * papeles);
         }
     }
     if(dinero > 0)
@@ -35,20 +44,30 @@ function entregarDinero()
     }
     else
     {
-        for(var e of entregado)
+      resultado.innerHTML += "<p>Retiraste:<br /></p>";
+  
+      for(var e of entregado)
+      {
+        if(e.cantidad > 0)
         {
-            if(e.cantidad > 0) {
-            resultado.innerHTML += + e.cantidad + ' billetes de $' + e.valor + '<br />';
-            }
+          for(var bi = 1; bi <= e.cantidad ;bi++)
+          {
+            resultado.innerHTML += "<img src=" + e.imagen.src + " width=200px />" + "<br /><hr />";
+          }
         }
+      }
     }
 }
 
+
 var caja = [];
-var entregado = [];
-caja.push( new Billete(50, 10) );
+caja.push( new Billete(500, 4,) );
+caja.push( new Billete(200, 5) );
+caja.push( new Billete(100, 10) );
+caja.push( new Billete(50, 20) );
 caja.push( new Billete(20, 30) );
-caja.push( new Billete(10, 10) );
+
+var entregado = [];
 
 var dinero;
 var div = 0;
@@ -56,5 +75,3 @@ var papeles = 0;
 var resultado = document.getElementById("resultado")
 var b = document.getElementById('extraer');
 b.addEventListener('click', entregarDinero);
-
-
